eb87f70a615c76d3bf284b568f0e50bec9b00aa5
Pallavi Goyal <pallavi.goyal@hashedin.com>
2021-05-27 03:19:51 +0530

    Tracker

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TasksComponent } from './component/tasks/tasks.component';
+import { AboutComponent } from './component/about/about.component';
+
+const routes: Routes = [
+  {path:'',component:TasksComponent},
+  {path:'about',component:AboutComponent}
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
